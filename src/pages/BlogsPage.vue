f0ccd8f53bb8846cc8fa0ede2a12724b1d72c985
Zsannen Mariano <iam@zmores.dev>
2023-05-06 23:03:50 +0800

    Changed path to prod path

--- src/pages/BlogsPage.vue
+++ src/pages/BlogsPage.vue
@@ -9,6 +9,19 @@
       />
     </div>
   </div>
+    <q-breadcrumbs class="text-grey mobile-hide">
+      <template v-slot:separator>
+        <q-icon size="1.0em" name="arrow_forward" color="grey" />
+      </template>
+
+      <q-breadcrumbs-el
+        class="text-grey"
+        label="Home"
+        to="https://zmores.dev/"
+      />
+      <q-breadcrumbs-el class="text-secondary" label="Blogs" />
+    </q-breadcrumbs>
+  
   <div class="section-blog-page justify-center row col full-width q-mt-xl">
     <div class="txt-intro text-secondary q-mt-xl">
       <p class="q-ma-none bebasneue-font txt-talk">✒️ My Awesome Blogs ✒️</p>
