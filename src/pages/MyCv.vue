4681447e4937a711e4db19dd612608f216d839fe
Zsannen Mariano <iam@zmores.dev>
2023-08-26 04:40:31 +0800

    Added senstim video for testing

--- src/pages/MyCv.vue
+++ src/pages/MyCv.vue
@@ -1,7 +1,24 @@
 <template>
-  <div class="section-blog-page justify-center row col full-width q-mt-xl">
-    <component :is="WorkExperiences" />
-  </div>
+  <q-page class="flex">
+    <div class="section-about-me self-start row col full-width q-py-lg q-mt-lg">
+      <div class="txt-intro col-7 col-md-7 col-xs-12 self-center">
+        <component :is="WorkExperiences" />
+      </div>
+    </div>
+
+    <q-page-sticky position="top-right" :offset="[90, 70]">
+      <q-card dark>
+        <q-card-section class="text-caption text-center">
+          Hi! This is a test that I would like to try.
+          This page has been designed with<br/>sensory stimulation in mind, ensuring that every user feels
+          comfortable <br/>and keeps them focused while exploring our content.
+          <br/><br/>
+          Kindly hit the play button to start the video and explore the rest of the page contents.
+        </q-card-section>
+      </q-card>
+      <iframe src="https://www.youtube.com/embed/n_Dv4JMiwK8?start=20&autoplay=1&loop=1&rel=0&cc_load_policy=1&iv_load_policy=3&fs=0&color=white&controls=0&disablekb=1&playlist=n_Dv4JMiwK8" width="560" height="315" title="1 hour 20 minutes of relaxing Minecraft Parkour (Nostalgia, Scenery, No Ads)" frameborder="0"></iframe>
+    </q-page-sticky>
+  </q-page>
 </template>
 
 <script>
